b9ce400757c20024da4c561756db2265ff197199
DanielSantamariaAlvarez <47115911+DanielSantamariaAlvarez@users.noreply.github.com>
2022-06-01 07:58:04 -0500

    toggle funcionando

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -1,8 +1,11 @@
 //* ---------------------- Daniel ---------------------- */
 
+const toggle = document.getElementById("toggle")
+const nav = document.getElementById("nav-menu")
 
-
-
+toggle.addEventListener("click", ()=>{
+    nav.classList.toggle("nav-visible")
+})
 
 
 
