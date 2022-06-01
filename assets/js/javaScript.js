09fe0200d90bca126b88bef6af8aaae936dc8738
DanielSantamariaAlvarez <47115911+DanielSantamariaAlvarez@users.noreply.github.com>
2022-06-01 08:16:30 -0500

    errores en menu corregidos

--- assets/js/javaScript.js
+++ assets/js/javaScript.js
@@ -1,11 +1,17 @@
 //* ---------------------- Daniel ---------------------- */
 
-const toggle = document.getElementById("toggle")
-const nav = document.getElementById("nav-menu")
-
-toggle.addEventListener("click", ()=>{
+const toggle = document.getElementById("toggle");
+const nav = document.getElementById("nav-menu");
+const links = document.querySelectorAll(".nlinks");
+let togglear = () =>{
     nav.classList.toggle("nav-visible")
-})
+}
+
+toggle.addEventListener("click", togglear);
+for (let index = 0; index < links.length; index++) {
+    links[index].addEventListener("click", togglear);
+    
+}
 
 
 
